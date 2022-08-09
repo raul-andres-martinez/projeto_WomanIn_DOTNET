8ac022baec7e8d23f07abaad6f99697a263a5c03
Nick Toledo <nickolasptoledo@gmail.com>
2022-08-09 11:23:26 -0300

    @Nick-Toledo [Task 3: Arquivo Posts] {11:23 09/08/2022}

---
+++ WomanIn/Src/Models/PostModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WomanInAPI.Src.Models
+{
+    [Table("tb_posts")]
+    public class PostModel
+    {
+        #region Atributos
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("FK_Creator")]
+        public string FK_Creator { get; set; }
+        [ForeignKey("FK_Category")]
+        public string FK_Category { get; set; }
+        public string PostDate { get; set; }
+
+        #endregion
+    }
+
+}
--- WomanIn/Src/Models/UserModel.cs
+++ WomanIn/Src/Models/UserModel.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WomanInAPI.Src.Models
 {
-    [Table("tb_usuarios")]
+    [Table("tb_Users")]
     public class UserModel
     {
         #region Atributos
@@ -15,6 +17,10 @@ namespace WomanInAPI.Src.Models
         public string Password { get; set; }
         public string CPF_CNPJ { get; set; }
         public string Area { get; set; }
+
+        [JsonIgnore, InverseProperty("FK_Creator")]
+        public List<PostModel> MyPosts { get; set; }
+
 
         #endregion
     }
